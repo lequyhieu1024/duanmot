8c831da9d82cb2536b431f1f2f3f72cabba30a2f
Hoang Cao Huan (FPL HN) <huanhcph35218@fpt.edu.vn>
2023-11-16 14:20:32 +0700

    sửa sql

--- app/views/Client/layout/home.php
+++ app/views/Client/layout/home.php
@@ -1,13 +1,13 @@
-home
-<a href="index.php?redirect=login">đăng nhập</a>
-<a href="index.php?redirect=logout">đăng xuất</a>
-<a href="index.php?redirect=unsetcookie">đăng xuất vĩnh viễn</a>
+<h1>HOME</h1>
+<a href="index.php?redirect=login">đăng nhập</a><br>
+<a href="index.php?redirect=logout">đăng xuất</a><br>
+<a href="index.php?redirect=unsetcookie">đăng xuất vĩnh viễn</a><br>
 <?php echo $_SESSION['id_role']; echo "<br/>";
  echo $_SESSION['ten_tai_khoan']; echo "<br/>";?>
  =============================================
  <?php 
  if($_SESSION['id_role']==3){
-    echo '<a href="app/views/admin/index.php">đến tragn quản trị viên</a> ';
+    echo '<a href="app/views/admin/index.php">Đến trang quản trị viên</a> ';
  }
  include'app/models/clientmodel/loadallsp.php';
  ?>
