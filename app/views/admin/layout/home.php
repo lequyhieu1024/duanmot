85d75e736e4070720ea2adea7466a8f99a77c8d6
lequyhieu1024 <lequyhieu1024@gmail.com>
2023-11-18 15:42:50 +0700

    sửa header.php

--- app/views/admin/layout/home.php
+++ app/views/admin/layout/home.php
@@ -48,20 +48,4 @@
               <a href="#" class="small-box-footer">Thêm thông tin <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h3>65</h3>
-
-                <p>Điểm Trang Web</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-              </div>
-              <a href="#" class="small-box-footer">Thêm thông tin <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
         </div>
