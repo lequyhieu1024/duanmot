251cab87da4904e31352933dbd8b013e8e7e7494
Hoang Cao Huan (FPL HN) <huanhcph35218@fpt.edu.vn>
2023-12-01 21:16:54 +0700

    chỉnh hiển thị thông tin tài khoản

--- app/views/Client/giangvien/chitietgiangvien.php
+++ app/views/Client/giangvien/chitietgiangvien.php
@@ -7,11 +7,11 @@ foreach($gv as $gv){
     <div class="col-md-12">
       <div class="card">
           <ul class="list-group list-group-flush text-left">
-          <li class="list-group-item"><strong>Ảnh đại diện:</strong> <span id="loginname"><img src="public/images/<?=$avt?>" alt="Ảnh đại diện" class="img-fluid mb-3 col-md-2"></span></li>
-            <li class="list-group-item"><strong>Họ và Tên:</strong> <h2 id="fullname"><?=$ten_giang_vien?></h2></li>
-            <li class="list-group-item"><strong>Email:</strong> <h2 id="email"><?=$email?></h2></li>
-            <li class="list-group-item"><strong>Số Điện Thoại:</strong> <h2 id="phone"><?=$so_dien_thoai?></h2></li>
-            <li class="list-group-item"><strong>Thông tin:</strong> <h2 id="phone"><?=$mo_ta?></h2></li>
+          <li class="list-group-item"><strong>Ảnh đại diện </strong> <span id="loginname"><img src="public/images/<?=$avt?>" alt="Ảnh đại diện" class="img-fluid mb-3 col-md-2"></span></li>
+            <li class="list-group-item"><strong>Họ và Tên</strong> <h2 id="fullname"><?=$ten_giang_vien?></h2></li>
+            <li class="list-group-item"><strong>Email</strong> <h2 id="email"><?=$email?></h2></li>
+            <li class="list-group-item"><strong>Số Điện Thoại</strong> <h2 id="phone"><?=$so_dien_thoai?></h2></li>
+            <li class="list-group-item"><strong>Thông tin</strong> <h2 id="phone"><?=$mo_ta?></h2></li>
           </ul>
         </div>
       </div>
--- app/views/Client/taikhoan/myinfo.php
+++ app/views/Client/taikhoan/myinfo.php
@@ -8,11 +8,11 @@ if($info != ""){
     <div class="col-md-12">
       <div class="card">
           <ul class="list-group list-group-flush text-left">
-          <li class="list-group-item"><strong>Ảnh đại diện:</strong> <span id="loginname"><img src="public/images/<?=$avt?>" alt="Ảnh đại diện" class="img-fluid mb-3 col-md-2"></span></li>
-          <li class="list-group-item"><strong>Tên Đăng Nhập:</strong> <span id="loginname"><?=$ten_tai_khoan?></span></li>
-            <li class="list-group-item"><strong>Họ và Tên:</strong> <span id="fullname"><?=$ho_va_ten?></span></li>
-            <li class="list-group-item"><strong>Email:</strong> <span id="email"><?=$email?></span></li>
-            <li class="list-group-item"><strong>Số Điện Thoại:</strong> <span id="phone"><?=$so_dien_thoai?></span></li>
+          <li class="list-group-item"><strong>Ảnh đại diện </strong> <span id="loginname"><img src="public/images/<?=$avt?>" alt="Ảnh đại diện" class="img-fluid mb-3 col-md-2"></span></li>
+          <li class="list-group-item"><strong>Tên Đăng Nhập</strong> <h2 id="loginname"><?=$ten_tai_khoan?></h2></li>
+            <li class="list-group-item"><strong>Họ và Tên</strong> <h2 id="fullname"><?=$ho_va_ten?></h2></li>
+            <li class="list-group-item"><strong>Email</strong> <h2 id="email"><?=$email?></h2></li>
+            <li class="list-group-item"><strong>Số Điện Thoại</strong> <h2 id="phone"><?=$so_dien_thoai?></h2></li>
             <li class="list-group-item"><strong>Mật Khẩu:</strong>
                 <?php $sl = strlen($mat_khau);
                 for ($i = 0; $i < $sl; $i++) {
